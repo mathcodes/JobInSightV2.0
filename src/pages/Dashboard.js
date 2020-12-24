6615760bf574e6ca78c7f2f82e234c124105ad6d
Jon Christie <fullstack11235@gmail.com>
2020-12-24 07:02:32 -0500

    flex options

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,39 +4,59 @@ import NavBar from "../components/NavBar";
 
 function Dashboard() {
   return (
-    <div class="container">
+    <div class="container-fluid">
       <NavBar />
       {/* -------- ROW -------- */}
       <div className="row">
-        <button
-          type="button"
-          class="btn btn-primary"
-          data-toggle="modal"
-          data-target="#exampleModal3"
-        >
-          Launch demo modal
-        </button>
+      <div className="col-sm Component">A2</div>
         </div>
 
         {/* -------- ROW -------- */}
         <div className="row">
-          <div className="shadow col col-md-3">
-            <div className="d-flex p-2 bd-highlight Component Component4">
-              Component4.b
-            </div>
-            </div></div>
+        <div className="col-sm Component">A2</div>
+        <div className="col-sm Component">A3</div>
+      </div>
+
         
         {/* -------- ROW -------- */}
         <div className="row">
-          <div className="col-md-2 Component">B1</div>
-          <div className="col-md-2 Component">B2</div>
-          <div className="col-md-8 Component">B3</div>
+          <div className="col-lg Component Component2">B1</div>
+          <div className="col-lg Component Component2">B2</div>
+          <div className="col-lg Component Component2">B3</div>
         </div>
 
-        <Footer />
-        </div>
-  
+         {/* -------- ROW -------- */}
+         <div className="row">
+         <div className="col-sm Component">B1</div>
+         <div className="col-sm Component">B2</div>
+         <div className="col-sm Component">B3</div>
+         <div className="col-sm Component">B4</div>
+       </div>
+
+       {/* -------- ROW -------- */}
+       <div className="row">
+       <div className="col-sm Component">
        
-)
-  }
+       </div>
+     </div>
+
+     {/* -------- ROW -------- */}
+     <div className="row rowB">
+     <div className="col col-sm Component Component1">B1</div>
+     <div className="col col-sm Component Component1"><div class="media">
+     <img src="../images/HBcolorsSM-xs.png" class="align-self-center mr-3" alt="OOO"/>
+     <div class="media-body">
+       <h5 class="mt-0">Center-aligned media</h5>
+       <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+       <p class="mb-0">Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+     </div>
+     </div>
+     </div>
+     </div>
+    
+
+  <Footer />
+  </div>
+  )}
+
 export default Dashboard;
