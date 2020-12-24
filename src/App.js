84dd67cd952d03890919c6e6eb7180b42d89683a
Jon Christie <fullstack11235@gmail.com>
2020-12-24 12:27:08 -0500

    modal

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 // import Login from './pages/Login'
 import Dashboard from './pages/Dashboard';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Search from "./pages/Search";
-import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 import "./App.css";
@@ -13,11 +11,11 @@ function App() {
   return (
     <Router>
       <div className="App App-header">
-        <NavBar />
+        
         <Wrapper>
         <Dashboard />
-          <Route exact path="/" component={Search} />
-         {/* <Route exact path="/#B1" component={B1} />*/}
+        <Route exact path="/" />
+        <Route exact path="#B1" />
         </Wrapper>
         <Footer />
       </div>
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -12,13 +12,23 @@ function Dashboard() {
         <header className="Header">User Name</header>
 
              </div>
-             <div className="col-sm Component Component2 imgComponent1">
-        <img className="" title="colors" href="../images/HBcolors.png"/>
+             <div className="col-sm Component imgComponent1">
+             <div class="card">
+             <div class="card-body">
+               <h4 class="card-title">Card title</h4>
+               <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+               <p class="card-text text-muted">
+                 Some quick example text to build on the card title
+                 and make up the bulk of the card's content.
+               </p>
+               <a href="#B1" class="card-link">Card link</a>
+               <a href="#D1" class="card-link">Another link</a>
+             </div>
+           </div>
 
              </div>
              <div className="col-sm Component inputComponent">
-        <header className="Header">User Name</header>
-
+              <header className="Header">User Name</header>
              </div>
       </div>
 
