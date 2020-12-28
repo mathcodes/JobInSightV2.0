c91feedbd6b91d4fc468a44eb2a0611e8ca73dd5
Jon Christie <fullstack11235@gmail.com>
2020-12-27 19:37:33 -0500

    stuff

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -20,80 +20,78 @@ function Dashboard() {
           </header>
         </div>
       </div>
-    {/* -------- ROW -------- */}
-    <div className="row jumbotron">
+      {/* -------- ROW -------- */}
+      <div className="row jumbotron">
         {/* -------- Calendar -------- */}
         <div className="col col-lg Component">
           <Calendar />
         </div>
-        
+
         {/* -------- YOUTUBE -------- */}
         <div className="col col-lg Component">
           <YouTube />
         </div>
-    </div>
-     
+      </div>
 
-        {/* -------- ROW -------- */}
-        <div className="row">
-           {/* -------- CARD -------- */}
-           <div className="col-sm Component imgComponent1">
-           <div class="card">
-             <div class="card-body">
-               <h4 class="card-title">Card title</h4>
-               <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-               <p class="card-text text-muted">
-                 Some quick example text to build on the card title and make up
-                 the bulk of the card's content.
-               </p>
-               <a href="#B1" class="card-link">
-                 Card link
-               </a>
-               <a href="#D1" class="card-link">
-                 Another link
-               </a>
-             </div>
-           </div>
-         </div>
-            {/* -------- CARDComponent -------- */}
+      {/* -------- ROW -------- */}
+      <div className="row">
+        {/* -------- CARD -------- */}
         <div className="col-sm Component imgComponent1">
-        <Card />
+          <div class="card">
+            <div class="card-body">
+              <h4 class="card-title">Card title</h4>
+              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p class="card-text text-muted">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#B1" class="card-link">
+                Card link
+              </a>
+              <a href="#D1" class="card-link">
+                Another link
+              </a>
+            </div>
+          </div>
+        </div>
+        {/* -------- CARDComponent -------- */}
+        <div className="col col-sm Component">
+          <Card />
+        </div>
+        {/* -------- WikiSearch -------- */}
+        <div className="col col-sm Component">
+          <WikiSearch />
+        </div>
+        {/* -------- Test -------- */}
+        <div className="col col-sm Component">
+          <Test />
+        </div>
       </div>
-          {/* -------- WikiSearch -------- */}
-          <div className="col col-sm Component">
-            <WikiSearch />
-          </div>
-          {/* -------- Test -------- */}
-          <div className="col col-sm Component">
-            <Test />
-          </div>
-        </div>
 
-        {/* -------- ROW -------- */}
-        <div className="row">
-          <div className="col col-sm Component">B2</div>
-        </div>
-
-        {/* -------- ROW -------- */}
-        <div className="row">
-          <div id="C1" className="col col-sm Component">
-            C1
-          </div>
-          <div className="col-sm Component">C2</div>
-          <div className="col-sm Component">C3</div>
-          <div className="col-sm Component">C4</div>
-        </div>
-
-        {/* -------- ROW -------- */}
-        <div className="row">
-          <div id="D1" className="col-sm Component">
-            D1
-          </div>
-        </div>
-
-        <Footer />
+      {/* -------- ROW -------- */}
+      <div className="row">
+        <div className="col col-sm Component">B2</div>
       </div>
-    
+
+      {/* -------- ROW -------- */}
+      <div className="row">
+        <div id="C1" className="col col-sm Component">
+          C1
+        </div>
+        <div className="col-sm Component">C2</div>
+        <div className="col-sm Component">C3</div>
+        <div className="col-sm Component">C4</div>
+      </div>
+
+      {/* -------- ROW -------- */}
+      <div className="row">
+        <div id="D1" className="col-sm Component">
+          D1
+        </div>
+      </div>
+
+      <Footer />
+    </div>
   );
 }
 
